d36cc72674353f8e3056257aecf5284909c50be0
Daniel Obasuyi <danielobasuyi@gmail.com>
2025-09-10 20:00:07 +0100

    chore(loader): replace custom overlay with ng-zorro nz-spin for global route loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,23 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router, RouterEvent, RouterOutlet, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
 import { SharedModule } from './shared/shared.module';
-import { LoaderComponent } from './shared/components/loader/loader.component';
 import { ErrorModalComponent } from './shared/components/error-modal/error-modal.component';
 import { LoaderService } from './core/services/loader.service';
 import { Subscription, filter } from 'rxjs';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SharedModule, LoaderComponent, ErrorModalComponent],
+  imports: [RouterOutlet, SharedModule, NzSpinModule, ErrorModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit, OnDestroy {
   title = 'My Aceroyal Admin';
   private routeSub = new Subscription();
+  private loaderSub = new Subscription();
+  spinning = false;
+  spinTip = '';
 
   constructor(private router: Router, private loader: LoaderService) {}
 
@@ -30,9 +33,16 @@ export class AppComponent implements OnInit, OnDestroy {
           this.loader.hide();
         }
       });
+
+    // Bind loader state to nz-spin
+    this.loaderSub = this.loader.loaderState$.subscribe(state => {
+      this.spinning = state.isLoading;
+      this.spinTip = state.message || '';
+    });
   }
 
   ngOnDestroy(): void {
     this.routeSub.unsubscribe();
+    this.loaderSub.unsubscribe();
   }
 }
