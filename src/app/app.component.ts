e0f5de01f05b6c767de7503c7b622c2f542ddbac
Daniel Obasuyi <danielobasuyi@gmail.com>
2025-09-10 19:57:49 +0100

    feat(loader): show global loader on route navigation; dark-mode styles for date picker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router, RouterEvent, RouterOutlet, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
 import { SharedModule } from './shared/shared.module';
 import { LoaderComponent } from './shared/components/loader/loader.component';
 import { ErrorModalComponent } from './shared/components/error-modal/error-modal.component';
+import { LoaderService } from './core/services/loader.service';
+import { Subscription, filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +12,27 @@ import { ErrorModalComponent } from './shared/components/error-modal/error-modal
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'My Aceroyal Admin';
+  private routeSub = new Subscription();
+
+  constructor(private router: Router, private loader: LoaderService) {}
+
+  ngOnInit(): void {
+    // Show a global loader during route transitions for better UX
+    this.routeSub = this.router.events
+      .pipe(filter((e: any): e is RouterEvent => !!e))
+      .subscribe((event) => {
+        if (event instanceof NavigationStart) {
+          this.loader.show('Loading view...');
+        }
+        if (event instanceof NavigationEnd || event instanceof NavigationCancel || event instanceof NavigationError) {
+          this.loader.hide();
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.routeSub.unsubscribe();
+  }
 }
